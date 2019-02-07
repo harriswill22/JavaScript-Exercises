2fe0227b684896db878e719b7bb947d9b54a44cf
Will Harris <Harriswill22@yahoo.com>
2019-02-07 14:58:31 -0500

    Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case

--- functions.js
+++ functions.js
@@ -19,4 +19,15 @@
 // }
 // console.log(reverse("hat"));
 
-//  
+// Write a JavaScript function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. 
+
+function upperCase(str){
+    const array1 = str.split (' ')
+    let newArray1 = [];
+    
+    for (let i = 0; i < array1.length; i++){
+        newArray1.push(array1[i].charAt(0).toUpperCase()+array1[i].slice(1));
+    }
+    return newArray1.join(' ')
+}
+console.log(upperCase('whole lotta gang'));
