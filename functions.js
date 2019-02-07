051d77d67149bd5ee2ccc5068e02ca0df6592d09
Will Harris <Harriswill22@yahoo.com>
2019-02-07 14:38:28 -0500

    JavaScript function that checks whether a passed string is palindrome or not?

---
+++ functions.js
@@ -0,0 +1,22 @@
+//Write a JavaScript function that reverse a number
+// function reverseANumber(number) {
+//     number = number + ""
+//     return number.split("").reverse().join("")
+// }
+// console.log(reverseANumber(123345));
+
+//Write a JavaScript function that checks whether a passed string is palindrome or not?
+
+// function reverse(value) {
+//     value = value.toLowerCase()
+//     const rev = value.split("").reverse("").join("")
+//     if (value === rev) {
+//         return "palindrome"
+//     }
+//     else {
+//         return "is not palindrome"
+//     }
+// }
+// console.log(reverse("hat"));
+
+//  
