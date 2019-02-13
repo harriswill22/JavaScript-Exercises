b16f6fcdbf9df4de007f671b4b0897897e26610a
Will Harris <Harriswill22@yahoo.com>
2019-02-13 13:16:33 -0500

    sorts array

--- array.js
+++ array.js
@@ -1,8 +1,12 @@
-const is_array = input => {
-    if (toString.call(input) === '[object Array]')
-    return true;
-    return false;
-};
-console.log(is_array('hello'));
-console.log(is_array(['1,2']));
+// const is_array = input => {
+//     if (toString.call(input) === '[object Array]')
+//     return true;
+//     return false;
+// };
+// console.log(is_array('hello'));
+// console.log(is_array(['1,2']));
 
+// Write a JavaScript program to sort the items of an array.
+
+// let arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ].sort()
+// console.log(arr1);
