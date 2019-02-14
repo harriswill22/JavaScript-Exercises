5770ad96dd1a25a6c6852d9c1698d47de2a3b0b6
Will Harris <Harriswill22@yahoo.com>
2019-02-14 15:54:45 -0500

    Write a JavaScript function to clone an array.

--- array.js
+++ array.js
@@ -12,13 +12,18 @@
 // console.log(arr1);
 
 //Write a JavaScript program which prints the elements of the following array.
-const a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+// const a = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
 
-for (const i in a) {
-    console.log(`row ${i}`);
-    for (const j in a[i])
-    {
-        console.log(` ${a[i][j]}`);
+// for (const i in a) {
+//     console.log(`row ${i}`);
+//     for (const j in a[i])
+//     {
+//         console.log(` ${a[i][j]}`);
         
-    }
-}
+//     }
+// }
+
+// Write a JavaScript function to clone an array.
+var array_Clone = arra1 => arra1.slice(0);
+console.log(array_Clone([1, 2, 4, 0]));
+console.log(array_Clone([1, 2, [4, 0]])); 
