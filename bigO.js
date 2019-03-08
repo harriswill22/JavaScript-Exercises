a7455989307f0fdda833e26f73d4b009456acf4b
Will Harris <Harriswill22@yahoo.com>
2019-03-08 10:26:41 -0500

    big O notation

--- bigO.js
+++ bigO.js
@@ -1,14 +1,14 @@
 // Suppose we want to write a function that calculates the sum of all numbers from 1 up to some number n
-function addUpTo(n) {
-    let total = 0;
-    for( let i = 1; i <= n; i++) {
-        total += i;
-    }
-    return total;
-}
-// console.log(addUpTo(5));
+// function addUpTo(n) {
+//     let total = 0;
+//     for( let i = 1; i <= n; i++) {
+//         total += i;
+//     }
+//     return total;
+// }
+// // console.log(addUpTo(5));
 
-function addUpTo(n) {
-    return n *(n+1)/2;
-}
+// function addUpTo(n) {
+//     return n *(n+1)/2;
+// }
 
